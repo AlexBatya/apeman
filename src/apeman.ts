c8b13ddcad36eba10f675f67448c9817758284e7
AlexBatya <alex28.12.12@mail.ru>
2023-10-07 20:01:34 +1000

    'new'

---
+++ src/apeman.ts
@@ -0,0 +1,47 @@
+import path from 'path';
+import fs from 'fs';
+import color from 'colors';
+import {deletePattern, createPattern, downloadPatter, updatePattern, checkAllPatter, checkPatter} from './axios';
+
+(async () => {
+    if(process.argv.includes('--download') || process.argv.includes('-d')){
+        const req: any = await downloadPatter(process.argv[2]);
+        console.log(req);
+    }
+    if(process.argv.includes('--upload') || process.argv.includes('-u')){
+        const req: any = await createPattern(process.argv[2]);
+        console.log(req);
+    }
+    if(process.argv.includes('--remove') || process.argv.includes('-r')){
+        const req: any = await deletePattern(process.argv[2]);
+        console.log(req)
+    }
+    if(process.argv.includes('--update') || process.argv.includes('-e')){
+        const req: any = await updatePattern(process.argv[2]);
+        console.log(req);
+    }
+    if(process.argv.includes('--check') || process.argv.includes('-c')){
+        const req: any = await checkPatter(process.argv[2]);
+        console.log(req);
+    }
+    if(process.argv.includes('--checkAll') || process.argv.includes('-a')){
+        const req: any = await checkAllPatter();
+        for(let elem of req){
+            console.log(elem);
+        }
+        console.log("Колличество пакетов в базе: " + req.length)
+    }
+    if(process.argv.includes('--help') || process.argv.includes('-h')){
+        console.log(`Использование apeman [-a] [-r] [-d] [-r] [-c] [-e] [-u]/n
+                     [<папка> -u, -d, -r]\n\n
+Парраметры: \n
+  -h --help          Инструкция\n
+  -u --upload        Загрузить новый паттерн: apeman <папка> -u\n
+  -d --download      Скачать паттерн: apeman <папка> -d\n
+  -e --update        Обновить паттерн на сервере: apeman <папка> -e\n
+  -r --remove        Удалить паттерн: apeman <папка> -r\n
+  -c --check         Проверить есть ли такой паттерн: apeman <папка> -c\n
+  -a --checkAll      Посмотреть все паттерны на сервере: apeman -a\n
+  `)
+    }
+})();
---
+++ src/axios.ts
@@ -0,0 +1,182 @@
+import path from 'path';
+import fs from 'fs';
+import color from 'colors';
+
+import axios from 'axios';
+
+const zl = require('zip-lib');
+
+const urlAll = 'http://localhost:3000';
+const token = 'asfsadfsh239412934sdkafasdcxvxkjlAKsdfcxaldfjd'
+
+interface data {
+    name: string,
+    object?: any
+}
+
+const axiosConfigPOST = (elem: any, url: string) => {
+    return {
+        method: 'POST',
+        url: urlAll + url, 
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': token 
+        },
+        data: JSON.stringify(elem)
+    }
+};
+
+const axiosConfigGET = (url: string) => {
+    return {
+        method: 'GET',
+        url: urlAll + url, 
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': token 
+        },
+    }
+};
+
+const axiosConfigDELETE = (elem: any, url: any) => {
+    return {
+        method: 'DELETE',
+        url: urlAll + url, 
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': token 
+        },
+        data: JSON.stringify(elem)
+    }
+};
+
+class Zipping{
+    dirLink: any = null; 
+    fileZip: any = null;
+
+    async create(){ 
+        return new Promise(async (res: any) => {
+            await zl.archiveFolder(this.dirLink, this.fileZip)
+            res()
+        })
+    }
+
+};
+
+export function createPattern(name: string){
+    return new Promise(async (res: any) => {
+        const dir = './'
+
+        const zip = new Zipping();
+        zip.dirLink = dir + name 
+        zip.fileZip = dir + name + '.zip';
+
+
+        await zip.create();
+
+
+        const readStream = fs.createReadStream(zip.fileZip);
+        const chunks: any[] = [];
+        readStream.on('data', chunk => chunks.push(chunk))
+        readStream.on('end', async () => {
+            const data: data = {
+                name: name,
+                object: Buffer.concat(chunks)
+            } 
+
+            const reqData = await axios(axiosConfigPOST(data, '/api/patterns/add'))
+            fs.unlinkSync(zip.fileZip)
+            res(reqData.data);
+        })
+    })
+    
+}
+
+export function updatePattern(name: string){
+    return new Promise(async (res: any) => {
+        const dir = './'
+
+        const zip = new Zipping();
+        zip.dirLink = dir + name 
+        zip.fileZip = dir + name + '.zip';
+
+
+        await zip.create();
+
+
+        const readStream = fs.createReadStream(zip.fileZip);
+        const chunks: any[] = [];
+        readStream.on('data', chunk => chunks.push(chunk))
+        readStream.on('end', async () => {
+            const data: data = {
+                name: name,
+                object: Buffer.concat(chunks)
+            } 
+
+            const reqData = await axios(axiosConfigPOST(data, '/api/patterns/update'))
+            fs.unlinkSync(zip.fileZip)
+            res(reqData.data);
+        })
+    })
+    
+}
+
+export async function deletePattern(name: string){
+    return new Promise( async (res: any) => {
+        const data: data = {
+            name: name
+        }
+        const reqData = await axios(axiosConfigDELETE(data, '/api/patterns/delete'))
+        res(reqData.data)
+    })
+    
+}
+
+export async function downloadPatter(name: string){
+    return new Promise(async (res: any) => { 
+        const dir = process.cwd(); 
+
+        const data = {
+            name: name
+        }
+
+
+        const reqData = await axios(axiosConfigPOST(data, '/api/patterns/download'))
+
+        if(reqData.data.status == 200){
+            await new Promise((res: any) => {
+                const writeStream = fs.createWriteStream(path.join(dir, 'downloadArchiv.zip'));
+                writeStream.write(Buffer.from(reqData.data.archiv))
+                res()
+            })
+
+            await zl.extract(path.join(dir, 'downloadArchiv.zip'), path.resolve(dir, name))
+            fs.unlinkSync(path.join(dir, 'downloadArchiv.zip'))
+            res(reqData.data.answer)
+        }
+        else{
+            res(reqData.data)
+        }
+
+        
+    })
+        
+}
+
+export async function checkPatter(name: string){
+    return new Promise(async (res: any) => {
+
+        const data: data = {
+            name: name
+        }
+
+        const reqData = await axios(axiosConfigPOST(data, '/api/patterns/check'))
+        res(reqData.data)
+    })
+}
+export async function checkAllPatter(){
+    return new Promise(async (res: any) => {
+
+        const reqData = await axios(axiosConfigGET('/api/patterns/checkAll'))
+        res(reqData.data)
+    })
+}
